15fb81ebaea8acba4307d716ddd17e8a0df97f10
git tool <git.hub@d8i.com>
2018-07-11 17:27:56 -0700

    forgot to remove unused function

--- reddit.py
+++ reddit.py
@@ -59,26 +59,6 @@ def md5(fname):
       hash_md5.update(chunk)
   return hash_md5.hexdigest()
 
-#https://www.peterbe.com/plog/best-practice-with-retries-with-requests
-def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
-    session=None,
-):
-    session = session or requests.Session()
-    retry = requests.packages.urllib3.util.retry.Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = requests.adapters.HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
-
 def verifyCreateDir(user, dltype):
   if not os.path.exists(user + "/" + dltype):
     try:
